5b5065a253108405ee529175c42b8d093522d6b1
hisham <4pa22cs048@pace.edu.in>
2025-03-09 13:34:10 +0530

    init commit

---
+++ src/com/kunal/Man.java
@@ -0,0 +1,12 @@
+package com.kunal;
+
+public class Man {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+    static String greet() {
+        String greeting = "Hello World";
+        return greeting;
+    }
+}
